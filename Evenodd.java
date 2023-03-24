7dfddef3ad7cd546affbdd211f9dda452812db11
Ajaytrimukhe <81799829+Ajaytrimukhe@users.noreply.github.com>
2023-03-24 10:37:34 +0530

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,19 @@
+// Create a Even Or Odd Number Program.
+ 
+class Evenodd
+{
+	public static void main(String args[])
+	{
+	int a=10;
+	
+	if(a%2 == 0)   /* Create a Conditon */
+	{
+		System.out.println("It is a Even Number");
+	}
+	else
+	{
+		System.out.println("It is a Odd Number");
+	}
+	
+	}
+}
